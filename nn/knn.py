81fa80510431e487176e577cebd7bf81a55d4884
chris <christianomachado10@gmail.com>
2020-03-15 00:55:45 -0300

    KNN for regression fix

--- nn/knn.py
+++ nn/knn.py
@@ -5,14 +5,18 @@ import torch
 # torch.linespace
 
 class KNN(object):
-    def __init__(self, K, samples_train, samples_targets):
+    def __init__(self, K, samples_train, samples_targets, regression=False):
         self.K = K
         self.samples = samples_train
         self.targets = samples_targets
+        self.regression = regression
 
     def predict(self, x):
         cdists = torch.cdist(x, self.samples)
         idxs = cdists.argsort()[:, :self.K]
         knns = self.targets[idxs]
 
+        if self.regression:
+            return knns.mean(dim=1)
+        
         return knns.mode(dim=1).values
