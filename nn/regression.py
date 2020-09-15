ab7784cefdb42b5059277e0a5aa08ba7802ab4cd
Chris Machado <christianomachado10@gmail.com>
2020-09-15 10:40:08 -0300

    feat: add logistic regression

--- nn/regression.py
+++ nn/regression.py
@@ -1,5 +1,91 @@
 import torch
+
+class LogisticRegression(object):
+    def __init__(self, x_train, x_test, y_train, y_test, digits, learning_rate=1e-3):
+        self.x_train = torch.tensor(x_train, dtype=float)
+        self.x_test = torch.tensor(x_test, dtype=float)
+
+        self.y_train = torch.reshape(torch.tensor(y_train, dtype=float), (y_train.shape[0], 1))
+        self.y_test = torch.reshape(torch.tensor(y_test, dtype=float), (y_test.shape[0], 1))
         
+        self.lr = learning_rate
+        self.digits = digits     
+
+    def __filter(self, train_or_test):
+        if train_or_test == 'train':
+            x, y = self.x_train, self.y_train
+        else:
+            x, y = self.x_test, self.y_test
+
+        digit1, digit2 = self.digits
+        
+        idx_digit1, idx_digit2, idx_nondigit = [], [], []
+        
+        for idx in range(y.shape[0]):
+            if digit1 == y[idx]:
+                idx_digit1.append(idx)
+            elif digit2 == y[idx]:
+                idx_digit2.append(idx)
+            else:
+                idx_nondigit.append(idx)
+
+        return idx_digit1, idx_digit2, idx_nondigit
+
+    def __label_to_class(self, train_or_test='train'):
+        idx1, idx2, _ = self.__filter(train_or_test=train_or_test)
+        
+        if train_or_test == 'train':
+            x = self.x_train.clone().detach()
+            y = self.y_train.clone().detach()
+        else:
+            x = self.x_test.clone().detach()
+            y = self.y_test.clone().detach()
+
+        for idx in idx1:
+            y[idx] = 1
+
+        for idx in idx2:
+            y[idx] = -1
+
+        idxs = idx1 + idx2
+        idxs.sort()
+        x = x[idxs]
+        y = y[idxs]
+
+        return x, y
+
+    def fit(self, stop=10):
+        x, y = self.__label_to_class(train_or_test='train')
+
+        theta = torch.zeros((x.shape[1], 1), dtype=float)
+
+        for _ in range(stop):
+            g = torch.mean((y * x) / (1 + torch.exp( y * (x @ theta))), 
+                            axis=0, 
+                            keepdim=True).T
+            theta += self.lr*g
+    
+        return theta
+
+    def predict(self, theta):
+        x, _ = self.__label_to_class(train_or_test='test')
+
+        y = 1 / (1 + torch.exp(-x @ theta))
+
+        ones = torch.ones(y.shape[0], 1)
+
+        return torch.where(y >= 0.5, ones, -ones)
+        
+    def acc(self, ypred):
+        _, y = self.__label_to_class(train_or_test='test')
+        hitrate = 0
+        for i in range(y.shape[0]):
+            if ypred[i] == y[i]:
+                hitrate += 1
+             
+        return hitrate * 100 / y.shape[0]
+        
+
 class Regression(object):
     def __init__(self, x, y, size=0.8, ones=True):
         x = torch.tensor(x)
@@ -24,8 +110,7 @@ class Regression(object):
         # return theta
         # @ → multiply matrix
         return torch.inverse(x.T @ x) @ x.T @ y
-    
-    
+     
     def predict(self, theta, x=None):
         if x is not None:
             return x @ theta
@@ -33,8 +118,7 @@ class Regression(object):
         x = self.x_test
         # return y predicted
         return x @ theta
-    
-        
+          
     def rmse(self, y_pred, y=None):
         if y is not None:
             return torch.sqrt(torch.mean((y - y_pred) ** 2))
